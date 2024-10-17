2a500639e2f059f7415f66cf38a973c9aa446eaf
AntonioP167 <aperezpacora@hotmail.com>
2024-10-16 20:55:00 -0500

    raiz y view,services

---
+++ Tayta/src/app/components/viewrole/viewrole.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-viewrole',
+  standalone: true,
+  imports: [],
+  templateUrl: './viewrole.component.html',
+  styleUrl: './viewrole.component.css'
+})
+export class ViewroleComponent {
+
+}
