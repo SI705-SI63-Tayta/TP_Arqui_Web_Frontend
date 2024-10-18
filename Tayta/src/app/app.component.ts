945006a5f5f49073de62ec446ef211bc313e0da6
Renzo Lenes <lenesrenzoalberto@gmail.com>
2024-10-17 21:07:29 -0500

    name env fix

--- Tayta/src/app/app.component.ts
+++ Tayta/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ListaruserComponent } from "./components/user/listaruser/listaruser.component";
 import { DailyactivitiesComponent } from "./components/dailyactivities/dailyactivities.component";
-import { ListarroleComponent } from "./components/role/listarrole/listarrole.component";
 import { RoleComponent } from "./components/role/role.component";
+import { UserComponent } from "./components/user/user.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ListaruserComponent, DailyactivitiesComponent, ListarroleComponent, RoleComponent],
+  imports: [RouterOutlet, DailyactivitiesComponent, RoleComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
