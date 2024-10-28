0071473938e765124162357e6854d9c23bac8664
Renzo Lenes <lenesrenzoalberto@gmail.com>
2024-10-27 21:44:17 -0500

    added toolbar and menu

--- Tayta/src/app/app.config.ts
+++ Tayta/src/app/app.config.ts
@@ -5,7 +5,11 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import {LayoutModule} from '@angular/cdk/layout'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync(),provideHttpClient(withFetch())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync(),provideHttpClient(withFetch()), provideAnimationsAsync(),LayoutModule]
 };
+
+
+
