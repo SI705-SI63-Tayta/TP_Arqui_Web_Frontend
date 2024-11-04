bcbb4206d654b9e336b4ae44e9a556a3a9f0efef
jairZJ26 <emerzonjairsosamacalupu@gmail.com>
2024-11-04 03:17:57 -0500

    notification y review
    crud

--- Tayta/src/app/services/review.service.ts
+++ Tayta/src/app/services/review.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environments';
 import { HttpClient } from '@angular/common/http';
 import { Review } from '../models/Review';
+import { map, Observable, Subject } from 'rxjs';
 
 const base_url=environment.base
 
@@ -11,19 +12,33 @@ const base_url=environment.base
 export class ReviewService {
 
   private url=`${base_url}/resenas`
-
-
-
-  constructor(private http:HttpClient) {  }
-
-
+  constructor(private http:HttpClient) { }
+  private listaCambio = new Subject<Review[]>();
   list(){
     return this.http.get<Review[]>(this.url);
   }
+  insert(r: Review) {
+    return this.http.post(this.url, r);
+  }
 
+  getList() {
+    return this.listaCambio.asObservable();
+  }
 
+  setList(listaNueva: Review[]) {
+    this.listaCambio.next(listaNueva);
+  }
 
+  delete(id: number) {
+    return this.http.delete(`${this.url}/${id}`);
+  }
 
+  listId(id: number) {
+    return this.http.get<Review>(`${this.url}/${id}`);
+  }
 
+  update(r: Review) {
+    return this.http.put(this.url, r);
+  }
 
 }
